3f0332da0ec9d29be698ea2b358d3ffd255c4999
maxFredenburgh <max.fredenburgh@gmail.com>
2019-11-29 11:27:30 -0500

    Bug Fixes

--- modules/custom/comment_spam/src/Form/ModuleConfigurationForm.php
+++ modules/custom/comment_spam/src/Form/ModuleConfigurationForm.php
@@ -35,27 +35,42 @@ class ModuleConfigurationForm extends ConfigFormBase {
         $config = $this->config('comment_spam.settings');
         $defaultWords = $config->get('default_list');
         $myWords = $config->get('custom_list');
+        $include_default = $config->get('include_default_list');
 
         $default_rows = "";
         foreach($defaultWords as $badWord){
             $default_rows.=$badWord."<br>";
         }
-
-        $form['default_list'] = array(
-            '#type' => 'radios',
-            '#title' => $this
-                ->t('Include default list?'),
-            '#default_value' => 0,
-            '#options' => array(
-                0 => $this
-                    ->t('Don\'t include'),
-                1 => $this
-                    ->t('Include'),
-            ),
-        );
+        if($include_default==1){
+            $form['default_list'] = array(
+                '#type' => 'radios',
+                '#title' => $this
+                    ->t('Include default list?'),
+                '#default_value' => 1,
+                '#options' => array(
+                    0 => $this
+                        ->t('Don\'t include'),
+                    1 => $this
+                        ->t('Include'),
+                ),
+            );
+        }else{
+            $form['default_list'] = array(
+                '#type' => 'radios',
+                '#title' => $this
+                    ->t('Include default list?'),
+                '#default_value' => 0,
+                '#options' => array(
+                    0 => $this
+                        ->t('Don\'t include'),
+                    1 => $this
+                        ->t('Include'),
+                ),
+            );
+        }
 
         $form['text_details']['#markup'] = t('<details><summary>Click here to view the default list</summary>'.$default_rows.'</details>');
-        $form['text_list_title']['#markup'] = t('<h3>Current list of banned words/phrases (select a word to remove it)</h3>');
+        $form['text_list_title']['#markup'] = t('<h3>Current list of banned words/phrases <br> (select checkbox beside a word to remove it)</h3>');
 
 
         $current_rows[]=array();
@@ -78,6 +93,12 @@ class ModuleConfigurationForm extends ConfigFormBase {
             '#empty' => $this
                 ->t('You have not added any words yet'),
         );
+        $form['update_list'] = array(
+            '#name' => 'update_list',
+            '#type' => 'submit',
+            '#value' => t('Remove Selected Words'),
+            '#submit' => array([$this, 'updateList']),
+        );
 
 /*        $form['block_pattern'] = array(
             '#type' => 'radios',
@@ -92,9 +113,9 @@ class ModuleConfigurationForm extends ConfigFormBase {
             ),
         );
 
-*/          $style="border: red solid; border-radius: 5px; padding:10px;";
-            $form['text_warning']['#markup'] =
-                t('<div style="'.$style.'"><h3 style="color: red">WARNING!</h3><br> Blocking sequence may produce unexpected results!<br>
+*/      $style="border: red solid; border-radius: 5px; padding:10px;";
+        $form['text_warning']['#markup'] =
+                t('<br><br><div style="'.$style.'"><h3 style="color: red">WARNING!</h3><br> Blocking sequence may produce unexpected results!<br>
            For example, banning \'ass\' will also block comments with the word \'classic\'. Be careful with what you ban!</div><br> ');
             $form['text_info']['#markup'] = t('<p>Not case sensitive</p>');
 
@@ -102,7 +123,30 @@ class ModuleConfigurationForm extends ConfigFormBase {
             '#type' => 'textarea',
             '#title' => $this->t('Add a word or words separated by line'),
         ];
+
+        $form['actions']['submit']['#value'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('words to list'),
+        ];
+
         return $form;
+    }
+
+    public function updateList(array &$form, FormStateInterface &$form_state) {
+        $config = $this->config('comment_spam.settings');
+        $result=$config->get('custom_list');
+        $wordsToRemove = array_filter($form_state->getValue('table'));
+
+
+        if(!empty($wordsToRemove)){
+            foreach ($wordsToRemove as $del_word){
+                if (($key = array_search($del_word, $result)) !== false) {
+                    unset($result[$key]);
+                }
+            }
+        }
+        $config->set('custom_list',$result)->save();
+        $form_state->setRebuild();
     }
 
     /**
@@ -120,17 +164,29 @@ class ModuleConfigurationForm extends ConfigFormBase {
         $default_list_include=$form_state->getValue('default_list');
         $wordsToRemove = array_filter($form_state->getValue('table'));
         $config = $this->config('comment_spam.settings');
+        $defaultWords = $config->get('default_list');
 
         $result=$config->get('custom_list');
+        if(!empty($result)){
+            if($default_list_include==1){
+                $result=array_merge($result,$defaultWords);
+            }else{
+                $result=array_diff($result,$defaultWords);
+            }
 
-        if($default_list_include==1){
-            $defaultWords = $config->get('default_list');
-            $result=array_merge($result,$defaultWords);
-        }
+            if(!empty($wordsToAdd)){
+                $wordsToAdd =explode("\n", $wordsToAdd);
+                $result=array_merge($result,$wordsToAdd);
+            }
+        }else{
+            if($default_list_include==1){
+                $result=$defaultWords;
+            }
 
-        if(!empty($wordsToAdd)){
-            $wordsToAdd =explode("\n", $wordsToAdd);
-            $result=array_merge($result,$wordsToAdd);
+            if(!empty($wordsToAdd)){
+                $wordsToAdd =explode("\n", $wordsToAdd);
+                $result=$wordsToAdd;
+            }
         }
 
         if(!empty($wordsToRemove)){
@@ -142,6 +198,8 @@ class ModuleConfigurationForm extends ConfigFormBase {
         }
 
         $config->set('custom_list',$result)->save();
+        $config->set('include_default_list',$default_list_include)->save();
+
     }
 
 }

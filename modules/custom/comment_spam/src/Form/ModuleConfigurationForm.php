c110ebbd42f02a7b2ca759a4f004d7826a99c589
maxFredenburgh <max.fredenburgh@gmail.com>
2020-01-07 12:04:15 -0500

    webform support

--- modules/custom/comment_spam/src/Form/ModuleConfigurationForm.php
+++ modules/custom/comment_spam/src/Form/ModuleConfigurationForm.php
@@ -32,47 +32,59 @@ class ModuleConfigurationForm extends ConfigFormBase {
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
         $form = parent::buildForm($form, $form_state);
+        //Get settings for module
         $config = $this->config('comment_spam.settings');
+
+        //Get list of all webforms
+        $entities = \Drupal::entityTypeManager()->getStorage('webform')->loadMultiple(NULL);
+        $webforms = array();
+        foreach( $entities as $entity_id => $entity ) {
+            $webforms[$entity_id] = $entity_id;
+        }
+
+        //Display current webforms with Spam Filter
+        $def_web = $config->get('webform_list');
+        $webform_list = array_diff($def_web,array('0'));
+        $form['webforms00']['#markup'] = t('<p>Webforms included: '.join(", ",$webform_list).'</p>');
+
+        //List to select webforms to add spam filter
+        $form['webforms01']['#markup'] = t('<details><summary>Add webforms</summary>');
+        $form['entities']=array(
+            '#type'=>'checkboxes',
+            '#options' => $webforms,
+            '#default_value' => $def_web,
+        );
+        $form['webforms02']['#markup'] = t('</details>');
+
+        //Get default list and full current list of words to filter by
         $defaultWords = $config->get('default_list');
         $myWords = $config->get('custom_list');
+        //Whether or not the default list is included
         $include_default = $config->get('include_default_list');
 
+        //Obtain and Display default list
         $default_rows = "";
         foreach($defaultWords as $badWord){
             $default_rows.=$badWord."<br>";
         }
-        if($include_default==1){
-            $form['default_list'] = array(
-                '#type' => 'radios',
-                '#title' => $this
-                    ->t('Include default list?'),
-                '#default_value' => 1,
-                '#options' => array(
-                    0 => $this
-                        ->t('Don\'t include'),
-                    1 => $this
-                        ->t('Include'),
-                ),
-            );
-        }else{
-            $form['default_list'] = array(
-                '#type' => 'radios',
-                '#title' => $this
-                    ->t('Include default list?'),
-                '#default_value' => 0,
-                '#options' => array(
-                    0 => $this
-                        ->t('Don\'t include'),
-                    1 => $this
-                        ->t('Include'),
-                ),
-            );
-        }
+
+        $form['default_list'] = array(
+            '#type' => 'radios',
+            '#title' => $this
+                ->t('Include default list?'),
+            '#default_value' => $include_default,
+            '#options' => array(
+                0 => $this
+                    ->t('Don\'t include'),
+                1 => $this
+                    ->t('Include'),
+            ),
+        );
 
         $form['text_details']['#markup'] = t('<details><summary>Click here to view the default list</summary>'.$default_rows.'</details>');
         $form['text_list_title']['#markup'] = t('<h3>Current list of banned words/phrases <br> (select checkbox beside a word to remove it)</h3>');
 
-
+        //Obtain and Display List of current list of banned words
         $current_rows[]=array();
 
         $count=1;
@@ -85,7 +97,7 @@ class ModuleConfigurationForm extends ConfigFormBase {
             'words' => $this
                 ->t('Bad word'),
         ];
-
+        //Display in tableselect to allow selection of words for removal
         $form['table'] = array(
             '#type' => 'tableselect',
             '#header' => $header,
@@ -93,6 +105,7 @@ class ModuleConfigurationForm extends ConfigFormBase {
             '#empty' => $this
                 ->t('You have not added any words yet'),
         );
+        //Remove Selected words
         $form['update_list'] = array(
             '#name' => 'update_list',
             '#type' => 'submit',
@@ -113,16 +126,18 @@ class ModuleConfigurationForm extends ConfigFormBase {
             ),
         );
 
-*/      $style="border: red solid; border-radius: 5px; padding:10px;";
+*/
+        $style="border: red solid; border-radius: 5px; padding:10px;";
         $form['text_warning']['#markup'] =
                 t('<br><br><div style="'.$style.'"><h3 style="color: red">WARNING!</h3><br> Blocking sequence may produce unexpected results!<br>
            For example, banning \'ass\' will also block comments with the word \'classic\'. Be careful with what you ban!</div><br> ');
             $form['text_info']['#markup'] = t('<p>Not case sensitive</p>');
 
-        $form['addWords'] = [
+            //Text area to add custom words to ban list
+            $form['addWords'] = [
             '#type' => 'textarea',
             '#title' => $this->t('Add a word or words separated by line'),
-        ];
+            ];
 
         $form['actions']['submit']['#value'] = [
             '#type' => 'submit',
@@ -132,6 +147,11 @@ class ModuleConfigurationForm extends ConfigFormBase {
         return $form;
     }
 
+    /**
+     * @param array $form
+     * @param FormStateInterface $form_state
+     * Remove words selected in tableselect from config 'custom_list'
+     */
     public function updateList(array &$form, FormStateInterface &$form_state) {
         $config = $this->config('comment_spam.settings');
         $result=$config->get('custom_list');
@@ -160,25 +180,34 @@ class ModuleConfigurationForm extends ConfigFormBase {
      * {@inheritdoc}
      */
     public function submitForm(array &$form, FormStateInterface $form_state){
+        //Obtain values submitted in form
         $wordsToAdd = $form_state->getValue('addWords');
         $default_list_include=$form_state->getValue('default_list');
         $wordsToRemove = array_filter($form_state->getValue('table'));
+        //Webforms selected in form
+        $webform_add = $form_state->getValue('entities');
+
+        //Obtain values in config before submitting form
         $config = $this->config('comment_spam.settings');
         $defaultWords = $config->get('default_list');
-
         $result=$config->get('custom_list');
+
+        //If the custom_list from config is not empty, merge old with new
         if(!empty($result)){
+            //Include default list
             if($default_list_include==1){
                 $result=array_merge($result,$defaultWords);
             }else{
                 $result=array_diff($result,$defaultWords);
             }
-
-            if(!empty($wordsToAdd)){
-                $wordsToAdd =explode("\n", $wordsToAdd);
-                $result=array_merge($result,$wordsToAdd);
+            //Add words from textarea
+            if(!empty($wordsToAdd)) {
+                $wordsToAdd = explode("\n", $wordsToAdd);
+                $result = array_merge($result, $wordsToAdd);
             }
-        }else{
+        }
+        //Else if empty, no need to merge
+        else{
             if($default_list_include==1){
                 $result=$defaultWords;
             }
@@ -197,9 +226,53 @@ class ModuleConfigurationForm extends ConfigFormBase {
             }
         }
 
+        //save results in config
         $config->set('custom_list',$result)->save();
         $config->set('include_default_list',$default_list_include)->save();
 
+        //Get list of current webforms
+        $current_webforms = $config->get('webform_list');
+        //Webforms that are selected to be added to list
+        $new_webforms = array_diff($webform_add,$current_webforms);
+        //Webforms that are deselected to be removed from list
+        $remove_webforms = array_diff($current_webforms,$webform_add);
+        //Save to config
+        $config->set('webform_list',$webform_add)->save();
+
+        //The spam_flag field to be added to webforms
+        $hidden_field = "\nspam_flag:\n  '#type': checkbox\n  '#title': spam_flag\n  '#disabled': true\n  '#wrapper_attributes':\n    class:\n      - hidden";
+        //For each webform to be added
+        if($new_webforms!=0){
+            foreach($new_webforms as $webform){
+                $wf_conf = \Drupal::configFactory()->getEditable('webform.webform.'.$webform);
+                //Add spam_flag field to element
+                $elements = $wf_conf->get('elements');
+                $elements.=$hidden_field;
+                $wf_conf->set('elements',$elements);
+                //If webform has email_notification handler, add condition to not send email if spam
+                $check_handler = $wf_conf->get('handlers.email_notification');
+                if($check_handler!=null){
+                    $wf_conf->set('handlers.email_notification.conditions.disabled.:input[name="spam_flag"].checked',true);
+                }
+                $wf_conf->save();
+            }
+        }
+        //For each webform to be removed
+        if($remove_webforms!=0){
+            foreach($remove_webforms as $webform){
+                $wf_conf = \Drupal::configFactory()->getEditable('webform.webform.'.$webform);
+                //Remove spam_flag field from elements
+                $elements = $wf_conf->get('elements');
+                $elements=str_replace($hidden_field,"",$elements);
+                $wf_conf->set('elements',$elements);
+                //Remove email_notification handler
+                $check_handler = $wf_conf->get('handlers.email_notification');
+                if($check_handler!=null){
+                    $wf_conf->set('handlers.email_notification.conditions',[]);
+                }
+                $wf_conf->save();
+            }
+        }
     }
 
 }

1e71c4da9476e342bb92b822e0676207db8577aa
RabiaSajjad <rabiasajjad.dev@gmail.com>
2022-07-08 16:55:19 -0400

    upgrade fgpviewer to v3.3.5

--- modules/custom/map/js/mapview.js
+++ modules/custom/map/js/mapview.js
@@ -9,36 +9,8 @@
 
   Drupal.behaviors.MapViewer = {
     attach: function (context, settings) {
-
-      if (navigator.appName == 'Microsoft Internet Explorer' ||
-          !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) ||
-          (typeof $.browser !== "undefined" && $.browser.msie == 1))
-      {
-        document.getElementById("ie").style.display = "block";
-        document.getElementById("open-data-map").style.display = "none";
-      }
-
-      function getQueryVariable(variable)
-      {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i=0;i<vars.length;i++) {
-          var pair = vars[i].split("=");
-          if(pair[0] === variable){return pair[1];}
-        }
-        return(false);
-      }
-
-      function bookmark(){
-        return new Promise(function (resolve) {
-          var thing = getQueryVariable("rv");
-          console.log(thing);
-          resolve(thing);
-        });
-      }
-
       var keys = drupalSettings.map_keys.split(',');
-      RV.getMap('open-data-map').restoreSession(keys);
+      RV.getMap('fgpmap').restoreSession(keys);
     }
   };
 
